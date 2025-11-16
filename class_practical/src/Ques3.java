3e215d02a731dca8fcbf1a3ab0bd3d7f447b08c2
Dipakhatri <dipakhatri778@gmail.com>
2025-11-16 08:29:57 +0545

    Question three

---
+++ class_practical/src/Ques3.java
@@ -0,0 +1,21 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+public class Ques3 {
+    public static void main(String[] args) {
+        try {
+            String ipAddress = "127.0.0.1";
+            InetAddress address = InetAddress.getByName(ipAddress);
+
+            System.out.println("IP Address:" + address.getHostAddress());
+
+            System.out.println("Is Loopback Address?" + address.isLoopbackAddress());
+            System.out.println("Is Any Local Address?" + address.isAnyLocalAddress());
+            System.out.println("Is Link Local Address?" + address.isLinkLocalAddress());
+            System.out.println("Is Site Local Address?" + address.isSiteLocalAddress());
+            System.out.println("Is Multicast Address?" + address.isMulticastAddress());
+        } catch (UnknownHostException e) {
+            System.out.println("Invalid Ip address");
+            e.printStackTrace();
+        }
+    }
+}
